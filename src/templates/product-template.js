275f2c0ca86d8edc31d8db4bd40594080d4a8c92
flopreynat <flopreynat@protonmail.com>
2020-04-12 01:19:14 +0200

    commit at endof39

--- src/templates/product-template.js
+++ src/templates/product-template.js
@@ -4,7 +4,7 @@ import Img from 'gatsby-image'
 
 import Layout from "../components/layout"
 
-const ProductTemplate = ({ data: { contentfulProduct} }) => (
+const ProductTemplate = ({ data: { contentfulProduct}, location }) => (
     <Layout>
 	    <div style={{
             marginLeft: '0 auto',
@@ -13,7 +13,17 @@ const ProductTemplate = ({ data: { contentfulProduct} }) => (
         }}
         >
     	    <h2>{contentfulProduct.name} - <span style={{ color: '#ccc'}}>Added on {contentfulProduct.createdAt}</span></h2>
+            <h4>${contentfulProduct.price}</h4>
             <p>{contentfulProduct.description}</p>
+            <button
+                className="snipcart-add-item"
+                data-item-id={contentfulProduct.slug}
+                data-item-price={contentfulProduct.price}
+                data-item-image={contentfulProduct.image.file.url}
+                data-item-name={contentfulProduct.name}
+                data-item-url={location.pathname}
+                >Add to Cart
+            </button>
             <Img style={{ 
                 margin: '0 auto',
                 maxWidth: 600 }}
@@ -26,12 +36,16 @@ export const query = graphql`
     query($slug: String!) {
         contentfulProduct(slug: {eq: $slug}) {
         name
+        slug
         price
         description
         createdAt(formatString: "MMMM Do, YYYY, h:mm:ss a")
         image {
             fluid(maxWidth: 800) {
                 ...GatsbyContentfulFluid
+            }
+            file {
+                url
             }
         }
     }
