a02da32f1295b5437c7abad9c361a3c86403e15c
flopreynat <flopreynat@protonmail.com>
2020-04-11 17:33:55 +0200

    commit at endof37

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,6 +3,8 @@ const { createFilePath } = require('gatsby-source-filesystem')
 
 const PostTemplate = path.resolve('./src/templates/post-template.js')
 const BlogTemplate = path.resolve('./src/templates/blog-template.js')
+const ProductTemplate = path.resolve('./src/templates/product-template.js')
+
 
 exports.onCreateNode = ({ node, getNode, actions }) => {
 	const { createNodeField } = actions
@@ -29,6 +31,16 @@ exports.createPages = async ({ graphql, actions }) => {
 		      }
 		    }
 		  }
+		  
+			allContentfulProduct {
+			  totalCount
+			  edges {
+				node {
+				  slug
+				}
+			  }
+			}
+		  
 		}
 	`)
 
@@ -60,6 +72,17 @@ exports.createPages = async ({ graphql, actions }) => {
 				isLastPage,
 				currentPage,
 				totalPages
+			}
+		})
+	})
+
+	const products = result.data.allContentfulProduct.edges
+	products.forEach(({ node: product }) => {
+		createPage({
+			path: `/products/${product.slug}`,
+			component: ProductTemplate,
+			context: {
+				slug: product.slug
 			}
 		})
 	})
