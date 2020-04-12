f5aad48d8236e4f41a276a2e5283ade2cc3c7f50
flopreynat <flopreynat@protonmail.com>
2020-04-12 19:17:28 +0200

    commit at endof40

--- src/components/header.js
+++ src/components/header.js
@@ -13,6 +13,9 @@ const Header = ({ siteTitle }) => (
   >
     <div
       style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between',
         margin: `0 auto`,
         maxWidth: 960,
         padding: `1.45rem 1.0875rem`,
@@ -38,6 +41,23 @@ const Header = ({ siteTitle }) => (
           </Link>
         </h1>
       </span>
+      {/* {shopping cart} */}
+      <div 
+        style={{
+          color: 'white',
+          cursor: 'pointer'
+        }}
+        className="snipcart-summary snipcart-checkout">
+        <div>
+          <strong> My Cart</strong>
+        </div>
+        <div>
+          <span style={{fontWeight: 'bold'}} className="snipcart-total-items" /> Items in Cart
+        </div>
+        <div>
+          Total price <span style={{fontWeight: 'bold'}} className="snipcart-total-price" />
+        </div>
+      </div>
     </div>
   </header>
 )
--- src/templates/product-template.js
+++ src/templates/product-template.js
@@ -16,6 +16,13 @@ const ProductTemplate = ({ data: { contentfulProduct}, location }) => (
             <h4>${contentfulProduct.price}</h4>
             <p>{contentfulProduct.description}</p>
             <button
+                style={{
+                    background: 'darkorange',
+                    color: 'white',
+                    padding: '0.3em',
+                    borderRadius: '5px',
+                    cursor: 'pointer',
+                }}
                 className="snipcart-add-item"
                 data-item-id={contentfulProduct.slug}
                 data-item-price={contentfulProduct.price}
